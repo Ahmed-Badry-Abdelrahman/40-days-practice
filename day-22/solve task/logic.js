4ca11ca2b09fee96ad90521f7155b9a90881e4af
Ahmed Badry <ra6514201@gmail.com>
2025-08-31 20:25:35 +0300

    solve task of day 22

---
+++ day-22/solve task/logic.js
@@ -0,0 +1,60 @@
+// function greet(user) {
+//   console.log(`welcome, ${user} in 40 days of js`);
+//   console.log(`thank you ${user} to join as`);
+// }
+// greet("Ahmed");
+
+// function calculator(a, b, operationCallback) {
+//   // Complete this function
+//   return operationCallback(a, b);
+// }
+
+// function add(x, y) {
+//   return x + y;
+// }
+// function subtract(x, y) {
+//   return x - y;
+// }
+
+// function multiply(x, y) {
+//   return x * y;
+// }
+
+// function divide(x, y) {
+//   return x / y;
+// }
+
+// const result = calculator(5, 3, multiply);
+// console.log(result);
+
+// function delayedMessage(message, delay, callback) {
+//   setTimeout(() => {
+//     console.log(message);
+//     callback ? callback() : null;
+//   }, delay);
+// }
+// delayedMessage("Task complete", 2000);
+
+// function filterNumbers(arr, conditionCallback) {
+//   console.log(arr.filter(conditionCallback));
+// }
+
+// filterNumbers([1, 2, 3, 4], (n) => n > 2); // should return [3, 4]
+
+// function task1(callback) {
+//   console.log("Task 1 done");
+//   callback();
+// }
+
+// function task2(callback) {
+//   console.log("Task 2 done");
+//   callback();
+// }
+
+// function task3() {
+//   console.log("Task 3 done");
+// }
+
+// task1(() => {
+//   task2(task3);
+// });
